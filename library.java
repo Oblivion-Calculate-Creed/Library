40ffad8593f07e0f811b4cf93bae346aad6be66a
Scottynic112 <71728095+Scottynic112@users.noreply.github.com>
2020-09-29 16:41:39 +0100

    Add files via upload

---
+++ library.java
@@ -0,0 +1,21 @@
+package qa.task;
+
+public class library {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Create a library system with functionality to manage items and people within
+		//Rules: 
+		//At least 1 abstract class, 
+		//Each item should have at least one additional attribute unique to itself
+		//Use of polymorphism 
+		//Correct usage of access modifiers 
+		//Naming conventions and commenting adhered to appropriately 
+		//At least 3 of: -Books, -Maps, -Government Docs, -Media Resource, -Newspaper, Magazines
+		//All of the following: Checkout, Check-in, Add, Remove, Update item
+		//All of the following: Register, Delete, Update person 
+		//Implement an ID system in your item class, utilising class and a static integer variable 
+
+	}
+
+}
